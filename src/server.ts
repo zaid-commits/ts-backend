6619b020528956ea08b54603afece8033f16ef7b
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-12-18 16:13:17 +0530

    upd: cors

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://percept-ai.vercel.app',
+    origin: ['https://percept-ai.vercel.app','http://localhost:5173'],
     methods: ['GET', 'POST'],
     credentials: true
   }
